4d1d6a887c4f1f40ea28451261830c39268f3a99
ballardmandy69 <149659216+ballardmandy69@users.noreply.github.com>
2025-08-07 00:55:11 +0800

    Update hkhk1.sh

--- hkhk1.sh
+++ hkhk1.sh
@@ -60,7 +60,7 @@ sysctl -w net.ipv4.tcp_dsack=1
 sysctl -w net.ipv4.tcp_timestamps=1
 sysctl -w net.ipv4.tcp_rfc1337=1
 sysctl -w net.ipv4.tcp_sack=1  
-sysctl -w net.ipv4.tcp_pacing_ss_ratio=300
+sysctl -w net.ipv4.tcp_pacing_ss_ratio=700
 
 tc qdisc replace dev ens5 root fq
 tc qdisc del dev ens5 root
