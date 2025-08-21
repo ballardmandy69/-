16f0c4c1319aabfa6a559cf5107013aeafc272aa
ballardmandy69 <149659216+ballardmandy69@users.noreply.github.com>
2025-08-21 15:49:21 +0800

    Update jpjp2.sh

--- jpjp2.sh
+++ jpjp2.sh
@@ -62,7 +62,8 @@ sysctl -w net.ipv4.tcp_dsack=1
 sysctl -w net.ipv4.tcp_timestamps=1
 sysctl -w net.ipv4.tcp_rfc1337=1
 sysctl -w net.ipv4.tcp_sack=1  
-sysctl -w net.ipv4.tcp_pacing_ss_ratio=700
+sysctl -w net.ipv4.tcp_pacing_ss_ratio=300
+sysctl -w net.ipv4.tcp_pacing_ca_ratio=150
 tc qdisc replace dev ens5 root fq
 tc qdisc del dev ens5 root
 tc -s qdisc show dev ens5
