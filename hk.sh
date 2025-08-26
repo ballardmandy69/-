fed700122ac1b8448f48e5bc31bb640356fada54
ballardmandy69 <149659216+ballardmandy69@users.noreply.github.com>
2025-08-27 01:18:58 +0800

    Update hk.sh

--- hk.sh
+++ hk.sh
@@ -57,8 +57,9 @@ sysctl -w net.ipv4.tcp_dsack=1
 sysctl -w net.ipv4.tcp_timestamps=1
 sysctl -w net.ipv4.tcp_rfc1337=1
 sysctl -w net.ipv4.tcp_sack=1  
-sysctl -w net.ipv4.tcp_pacing_ss_ratio=300
-sysctl -w net.ipv4.tcp_pacing_ca_ratio=150
+sysctl -w net.ipv4.tcp_pacing_ss_ratio=250
+sysctl -w net.ipv4.tcp_pacing_ca_ratio=130
+sysctl -w net.core.netdev_budget=3000
 tc qdisc replace dev ens5 root fq
 tc qdisc del dev ens5 root
 tc -s qdisc show dev ens5
