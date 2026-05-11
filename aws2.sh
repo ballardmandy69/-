fd22f8c2b52f97017134e67fe67fedc58d39f953
ballardmandy69 <149659216+ballardmandy69@users.noreply.github.com>
2026-05-11 10:49:28 +0800

    Update aws2.sh

--- aws2.sh
+++ aws2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-S=zumo OPTIMIZE=1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t f96ba212-3920-4160-a5bc-f5b2f218c50e -u https://zumo.moe"
+S=zumo  bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t f96ba212-3920-4160-a5bc-f5b2f218c50e -u https://zumo.moe"
 sysctl -w net.core.default_qdisc=fq
 sysctl -w net.ipv4.tcp_mem="31457280 39321600 47185920"
 sysctl -w net.ipv4.tcp_slow_start_after_idle=0
