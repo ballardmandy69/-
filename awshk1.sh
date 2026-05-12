28cf880fbf4c86a5ed1384e651fa6472751c91a2
ballardmandy69 <149659216+ballardmandy69@users.noreply.github.com>
2026-05-13 02:52:55 +0800

    Update awshk1.sh

--- awshk1.sh
+++ awshk1.sh
@@ -52,8 +52,8 @@ sysctl -w net.ipv4.tcp_pacing_ss_ratio=260
 sysctl -w net.ipv4.tcp_pacing_ca_ratio=120
 sysctl -w net.ipv4.tcp_notsent_lowat=131072   
 sysctl -w net.ipv4.tcp_mtu_probing=1
-sysctl -w net.ipv4.tcp_probe_interval=45
-sysctl -w net.ipv4.tcp_probe_threshold=6
+sysctl -w net.ipv4.tcp_probe_interval=60
+sysctl -w net.ipv4.tcp_probe_threshold=8
 
 
 
