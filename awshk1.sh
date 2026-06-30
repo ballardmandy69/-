64ca3af8608e5c313d67b1762c85f4c3a17f5d4a
ballardmandy69 <149659216+ballardmandy69@users.noreply.github.com>
2026-07-01 00:54:03 +0800

    Update awshk1.sh

--- awshk1.sh
+++ awshk1.sh
@@ -49,7 +49,6 @@ sysctl -w net.ipv4.tcp_no_metrics_save=1
 # IPv4 探测失败时，同时更换 IPv4 并重新分配一个 IPv6
 
 
-
 cat > /usr/local/bin/push_node_a.sh << 'EOF'
 #!/bin/bash
 
@@ -59,6 +58,8 @@ NODE_NAME_V4="node_a"
 NODE_NAME_V6="node_a6"
 CHECK_IP_V4="47.116.126.134"
 CHECK_IP_V6="2408:4002:1350:300:78f:ab91:32c0:615c"
+FAIL_THRESHOLD_V4=6
+FAIL_THRESHOLD_V6=6
 
 CHANGE_COOLDOWN=90
 LAST_CHANGE_FILE_V4="/tmp/node_a_last_change_ip"
@@ -246,21 +247,37 @@ while true; do
   IPV4=$(get_public_ipv4 | tr -d ' \n\r')
   IPV6=$(get_public_ipv6 | tr -d ' \n\r')
   REFRESH_IPS=false
+  [ -z "${V4_FAIL_COUNT+x}" ] && V4_FAIL_COUNT=0
+  [ -z "${V6_FAIL_COUNT+x}" ] && V6_FAIL_COUNT=0
 
   if check_ping_v4; then
+    V4_FAIL_COUNT=0
     PING_OK_V4=true
   else
-    PING_OK_V4=false
-    change_ipv4
-    REFRESH_IPS=true
+    V4_FAIL_COUNT=$((V4_FAIL_COUNT + 1))
+    log "node_a v4 ping failed count=${V4_FAIL_COUNT}/${FAIL_THRESHOLD_V4}"
+    if [ "$V4_FAIL_COUNT" -ge "$FAIL_THRESHOLD_V4" ]; then
+      PING_OK_V4=false
+      change_ipv4
+      REFRESH_IPS=true
+    else
+      PING_OK_V4=true
+    fi
   fi
 
   if check_ping_v6; then
+    V6_FAIL_COUNT=0
     PING_OK_V6=true
   else
-    PING_OK_V6=false
-    change_ipv6
-    REFRESH_IPS=true
+    V6_FAIL_COUNT=$((V6_FAIL_COUNT + 1))
+    log "node_a v6 ping failed count=${V6_FAIL_COUNT}/${FAIL_THRESHOLD_V6}"
+    if [ "$V6_FAIL_COUNT" -ge "$FAIL_THRESHOLD_V6" ]; then
+      PING_OK_V6=false
+      change_ipv6
+      REFRESH_IPS=true
+    else
+      PING_OK_V6=true
+    fi
   fi
 
   if [ "$REFRESH_IPS" = true ]; then
